f6a20819f4465a7cf4b79e34819fbe221ee1341c
siri200031 <148340425+siri200031@users.noreply.github.com>
2024-01-18 10:53:04 +0530

    first commit

---
+++ capstone/source code/Backend Project/backend-app/src/test/java/com/mAadhar/main/MAadharAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mAadhar.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MAadharAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
